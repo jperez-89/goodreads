b3512023e4b7d31d9bb7ad3c812f3509d8558768
Jairo <jrwc1989@gmail.com>
2025-09-26 10:29:35 -0600

    Fix: Ajuste en About

--- app/(tabs)/(about)/about.tsx
+++ app/(tabs)/(about)/about.tsx
@@ -1,96 +1,87 @@
 import React from "react";
-import { Link } from "expo-router";
-import { Text, ScrollView, Pressable } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, ScrollView } from "react-native";
+import { useTheme } from "@/theme/ThemeProvider";
 
 export default function About() {
+  const { theme } = useTheme();
 
   return (
-    <SafeAreaView>
-      <ScrollView alwaysBounceVertical={true} className="p-3 ">
-        <Text className="text-2xl font-bold">About</Text>
-        <Text className="mb-4 font-bold">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna
-          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-          aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-          aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          <Link asChild href={"/"}>
-            <Pressable>
-              <Text className="text-blue-500">Go back home</Text>
-            </Pressable>
-          </Link>
-        </Text>
-        <Text className="mb-4">
-          Ea incididunt dolore cupidatat aliqua dolore deserunt ea minim elit culpa occaecat irure. Laborum nostrud labore do minim
-          quis incididunt esse non in fugiat ea. Aliqua et aliquip non anim laboris sint in culpa. Commodo aute ut qui deserunt
-          cupidatat amet occaecat sint nulla sint tempor laboris aliqua adipisicing. Ex in pariatur laborum ad eiusmod aliquip
-          dolore nulla dolore sint reprehenderit ullamco. Veniam ullamco Lorem esse dolore amet id. Ea veniam elit laborum
-          adipisicing magna consequat culpa Lorem esse non commodo incididunt aliquip eu. Consequat cillum id pariatur officia qui
-          ullamco commodo duis commodo id eiusmod dolore magna. Nulla laborum irure occaecat in officia enim culpa deserunt do
-          aliqua voluptate sunt sunt. Labore occaecat duis Lorem quis. Do proident amet minim reprehenderit sint. Sunt ea ex
-          incididunt quis adipisicing ipsum laboris Lorem ullamco cupidatat ipsum. Nisi quis eu laborum dolore veniam quis nostrud
-          ullamco. Nulla ullamco sit nisi ea nostrud dolor ea aute.
-        </Text>
+    <ScrollView alwaysBounceVertical={true} className="p-3 ">
+      <Text className={`mb-4 text-justify font-bold ${theme.colors.textContent}`}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </Text>
+      <Text className={`mb-4 text-justify ${theme.colors.textContent}`}>
+        Ea incididunt dolore cupidatat aliqua dolore deserunt ea minim elit culpa occaecat irure. Laborum nostrud labore do minim
+        quis incididunt esse non in fugiat ea. Aliqua et aliquip non anim laboris sint in culpa. Commodo aute ut qui deserunt
+        cupidatat amet occaecat sint nulla sint tempor laboris aliqua adipisicing. Ex in pariatur laborum ad eiusmod aliquip
+        dolore nulla dolore sint reprehenderit ullamco. Veniam ullamco Lorem esse dolore amet id. Ea veniam elit laborum
+        adipisicing magna consequat culpa Lorem esse non commodo incididunt aliquip eu. Consequat cillum id pariatur officia qui
+        ullamco commodo duis commodo id eiusmod dolore magna. Nulla laborum irure occaecat in officia enim culpa deserunt do
+        aliqua voluptate sunt sunt. Labore occaecat duis Lorem quis. Do proident amet minim reprehenderit sint. Sunt ea ex
+        incididunt quis adipisicing ipsum laboris Lorem ullamco cupidatat ipsum. Nisi quis eu laborum dolore veniam quis nostrud
+        ullamco. Nulla ullamco sit nisi ea nostrud dolor ea aute.
+      </Text>
 
-        <Text className="mb-4">
-          Ea incididunt dolore cupidatat aliqua dolore deserunt ea minim elit culpa occaecat irure. Laborum nostrud labore do minim
-          quis incididunt esse non in fugiat ea. Aliqua et aliquip non anim laboris sint in culpa. Commodo aute ut qui deserunt
-          cupidatat amet occaecat sint nulla sint tempor laboris aliqua adipisicing. Ex in pariatur laborum ad eiusmod aliquip
-          dolore nulla dolore sint reprehenderit ullamco. Veniam ullamco Lorem esse dolore amet id. Ea veniam elit laborum
-          adipisicing magna consequat culpa Lorem esse non commodo incididunt aliquip eu. Consequat cillum id pariatur officia qui
-          ullamco commodo duis commodo id eiusmod dolore magna. Nulla laborum irure occaecat in officia enim culpa deserunt do
-          aliqua voluptate sunt sunt. Labore occaecat duis Lorem quis. Do proident amet minim reprehenderit sint. Sunt ea ex
-          incididunt quis adipisicing ipsum laboris Lorem ullamco cupidatat ipsum. Nisi quis eu laborum dolore veniam quis nostrud
-          ullamco. Nulla ullamco sit nisi ea nostrud dolor ea aute.
-        </Text>
+      <Text className={`mb-4 text-justify ${theme.colors.textContent}`}>
+        Ea incididunt dolore cupidatat aliqua dolore deserunt ea minim elit culpa occaecat irure. Laborum nostrud labore do minim
+        quis incididunt esse non in fugiat ea. Aliqua et aliquip non anim laboris sint in culpa. Commodo aute ut qui deserunt
+        cupidatat amet occaecat sint nulla sint tempor laboris aliqua adipisicing. Ex in pariatur laborum ad eiusmod aliquip
+        dolore nulla dolore sint reprehenderit ullamco. Veniam ullamco Lorem esse dolore amet id. Ea veniam elit laborum
+        adipisicing magna consequat culpa Lorem esse non commodo incididunt aliquip eu. Consequat cillum id pariatur officia qui
+        ullamco commodo duis commodo id eiusmod dolore magna. Nulla laborum irure occaecat in officia enim culpa deserunt do
+        aliqua voluptate sunt sunt. Labore occaecat duis Lorem quis. Do proident amet minim reprehenderit sint. Sunt ea ex
+        incididunt quis adipisicing ipsum laboris Lorem ullamco cupidatat ipsum. Nisi quis eu laborum dolore veniam quis nostrud
+        ullamco. Nulla ullamco sit nisi ea nostrud dolor ea aute.
+      </Text>
 
-        <Text className="mb-4">
-          Ea incididunt dolore cupidatat aliqua dolore deserunt ea minim elit culpa occaecat irure. Laborum nostrud labore do minim
-          quis incididunt esse non in fugiat ea. Aliqua et aliquip non anim laboris sint in culpa. Commodo aute ut qui deserunt
-          cupidatat amet occaecat sint nulla sint tempor laboris aliqua adipisicing. Ex in pariatur laborum ad eiusmod aliquip
-          dolore nulla dolore sint reprehenderit ullamco. Veniam ullamco Lorem esse dolore amet id. Ea veniam elit laborum
-          adipisicing magna consequat culpa Lorem esse non commodo incididunt aliquip eu. Consequat cillum id pariatur officia qui
-          ullamco commodo duis commodo id eiusmod dolore magna. Nulla laborum irure occaecat in officia enim culpa deserunt do
-          aliqua voluptate sunt sunt. Labore occaecat duis Lorem quis. Do proident amet minim reprehenderit sint. Sunt ea ex
-          incididunt quis adipisicing ipsum laboris Lorem ullamco cupidatat ipsum. Nisi quis eu laborum dolore veniam quis nostrud
-          ullamco. Nulla ullamco sit nisi ea nostrud dolor ea aute.
-        </Text>
-        <Text className="mb-4">
-          Ea incididunt dolore cupidatat aliqua dolore deserunt ea minim elit culpa occaecat irure. Laborum nostrud labore do minim
-          quis incididunt esse non in fugiat ea. Aliqua et aliquip non anim laboris sint in culpa. Commodo aute ut qui deserunt
-          cupidatat amet occaecat sint nulla sint tempor laboris aliqua adipisicing. Ex in pariatur laborum ad eiusmod aliquip
-          dolore nulla dolore sint reprehenderit ullamco. Veniam ullamco Lorem esse dolore amet id. Ea veniam elit laborum
-          adipisicing magna consequat culpa Lorem esse non commodo incididunt aliquip eu. Consequat cillum id pariatur officia qui
-          ullamco commodo duis commodo id eiusmod dolore magna. Nulla laborum irure occaecat in officia enim culpa deserunt do
-          aliqua voluptate sunt sunt. Labore occaecat duis Lorem quis. Do proident amet minim reprehenderit sint. Sunt ea ex
-          incididunt quis adipisicing ipsum laboris Lorem ullamco cupidatat ipsum. Nisi quis eu laborum dolore veniam quis nostrud
-          ullamco. Nulla ullamco sit nisi ea nostrud dolor ea aute.
-        </Text>
-        <Text className="mb-4">
-          Ea incididunt dolore cupidatat aliqua dolore deserunt ea minim elit culpa occaecat irure. Laborum nostrud labore do minim
-          quis incididunt esse non in fugiat ea. Aliqua et aliquip non anim laboris sint in culpa. Commodo aute ut qui deserunt
-          cupidatat amet occaecat sint nulla sint tempor laboris aliqua adipisicing. Ex in pariatur laborum ad eiusmod aliquip
-          dolore nulla dolore sint reprehenderit ullamco. Veniam ullamco Lorem esse dolore amet id. Ea veniam elit laborum
-          adipisicing magna consequat culpa Lorem esse non commodo incididunt aliquip eu. Consequat cillum id pariatur officia qui
-          ullamco commodo duis commodo id eiusmod dolore magna. Nulla laborum irure occaecat in officia enim culpa deserunt do
-          aliqua voluptate sunt sunt. Labore occaecat duis Lorem quis. Do proident amet minim reprehenderit sint. Sunt ea ex
-          incididunt quis adipisicing ipsum laboris Lorem ullamco cupidatat ipsum. Nisi quis eu laborum dolore veniam quis nostrud
-          ullamco. Nulla ullamco sit nisi ea nostrud dolor ea aute.
-        </Text>
-        <Text className="mb-4">
-          Ea incididunt dolore cupidatat aliqua dolore deserunt ea minim elit culpa occaecat irure. Laborum nostrud labore do minim
-          quis incididunt esse non in fugiat ea. Aliqua et aliquip non anim laboris sint in culpa. Commodo aute ut qui deserunt
-          cupidatat amet occaecat sint nulla sint tempor laboris aliqua adipisicing. Ex in pariatur laborum ad eiusmod aliquip
-          dolore nulla dolore sint reprehenderit ullamco. Veniam ullamco Lorem esse dolore amet id. Ea veniam elit laborum
-          adipisicing magna consequat culpa Lorem esse non commodo incididunt aliquip eu. Consequat cillum id pariatur officia qui
-          ullamco commodo duis commodo id eiusmod dolore magna. Nulla laborum irure occaecat in officia enim culpa deserunt do
-          aliqua voluptate sunt sunt. Labore occaecat duis Lorem quis. Do proident amet minim reprehenderit sint. Sunt ea ex
-          incididunt quis adipisicing ipsum laboris Lorem ullamco cupidatat ipsum. Nisi quis eu laborum dolore veniam quis nostrud
-          ullamco. Nulla ullamco sit nisi ea nostrud dolor ea aute.
-        </Text>
-      </ScrollView>
-    </SafeAreaView>
-
+      <Text className={`mb-4 text-justify ${theme.colors.textContent}`}>
+        Ea incididunt dolore cupidatat aliqua dolore deserunt ea minim elit culpa occaecat irure. Laborum nostrud labore do minim
+        quis incididunt esse non in fugiat ea. Aliqua et aliquip non anim laboris sint in culpa. Commodo aute ut qui deserunt
+        cupidatat amet occaecat sint nulla sint tempor laboris aliqua adipisicing. Ex in pariatur laborum ad eiusmod aliquip
+        dolore nulla dolore sint reprehenderit ullamco. Veniam ullamco Lorem esse dolore amet id. Ea veniam elit laborum
+        adipisicing magna consequat culpa Lorem esse non commodo incididunt aliquip eu. Consequat cillum id pariatur officia qui
+        ullamco commodo duis commodo id eiusmod dolore magna. Nulla laborum irure occaecat in officia enim culpa deserunt do
+        aliqua voluptate sunt sunt. Labore occaecat duis Lorem quis. Do proident amet minim reprehenderit sint. Sunt ea ex
+        incididunt quis adipisicing ipsum laboris Lorem ullamco cupidatat ipsum. Nisi quis eu laborum dolore veniam quis nostrud
+        ullamco. Nulla ullamco sit nisi ea nostrud dolor ea aute.
+      </Text>
+      <Text className={`mb-4 text-justify ${theme.colors.textContent}`}>
+        Ea incididunt dolore cupidatat aliqua dolore deserunt ea minim elit culpa occaecat irure. Laborum nostrud labore do minim
+        quis incididunt esse non in fugiat ea. Aliqua et aliquip non anim laboris sint in culpa. Commodo aute ut qui deserunt
+        cupidatat amet occaecat sint nulla sint tempor laboris aliqua adipisicing. Ex in pariatur laborum ad eiusmod aliquip
+        dolore nulla dolore sint reprehenderit ullamco. Veniam ullamco Lorem esse dolore amet id. Ea veniam elit laborum
+        adipisicing magna consequat culpa Lorem esse non commodo incididunt aliquip eu. Consequat cillum id pariatur officia qui
+        ullamco commodo duis commodo id eiusmod dolore magna. Nulla laborum irure occaecat in officia enim culpa deserunt do
+        aliqua voluptate sunt sunt. Labore occaecat duis Lorem quis. Do proident amet minim reprehenderit sint. Sunt ea ex
+        incididunt quis adipisicing ipsum laboris Lorem ullamco cupidatat ipsum. Nisi quis eu laborum dolore veniam quis nostrud
+        ullamco. Nulla ullamco sit nisi ea nostrud dolor ea aute.
+      </Text>
+      <Text className={`mb-4 text-justify ${theme.colors.textContent}`}>
+        Ea incididunt dolore cupidatat aliqua dolore deserunt ea minim elit culpa occaecat irure. Laborum nostrud labore do minim
+        quis incididunt esse non in fugiat ea. Aliqua et aliquip non anim laboris sint in culpa. Commodo aute ut qui deserunt
+        cupidatat amet occaecat sint nulla sint tempor laboris aliqua adipisicing. Ex in pariatur laborum ad eiusmod aliquip
+        dolore nulla dolore sint reprehenderit ullamco. Veniam ullamco Lorem esse dolore amet id. Ea veniam elit laborum
+        adipisicing magna consequat culpa Lorem esse non commodo incididunt aliquip eu. Consequat cillum id pariatur officia qui
+        ullamco commodo duis commodo id eiusmod dolore magna. Nulla laborum irure occaecat in officia enim culpa deserunt do
+        aliqua voluptate sunt sunt. Labore occaecat duis Lorem quis. Do proident amet minim reprehenderit sint. Sunt ea ex
+        incididunt quis adipisicing ipsum laboris Lorem ullamco cupidatat ipsum. Nisi quis eu laborum dolore veniam quis nostrud
+        ullamco. Nulla ullamco sit nisi ea nostrud dolor ea aute.
+      </Text>
+      <Text className={`mb-4 text-justify ${theme.colors.textContent}`}>
+        Ea incididunt dolore cupidatat aliqua dolore deserunt ea minim elit culpa occaecat irure. Laborum nostrud labore do minim
+        quis incididunt esse non in fugiat ea. Aliqua et aliquip non anim laboris sint in culpa. Commodo aute ut qui deserunt
+        cupidatat amet occaecat sint nulla sint tempor laboris aliqua adipisicing. Ex in pariatur laborum ad eiusmod aliquip
+        dolore nulla dolore sint reprehenderit ullamco. Veniam ullamco Lorem esse dolore amet id. Ea veniam elit laborum
+        adipisicing magna consequat culpa Lorem esse non commodo incididunt aliquip eu. Consequat cillum id pariatur officia qui
+        ullamco commodo duis commodo id eiusmod dolore magna. Nulla laborum irure occaecat in officia enim culpa deserunt do
+        aliqua voluptate sunt sunt. Labore occaecat duis Lorem quis. Do proident amet minim reprehenderit sint. Sunt ea ex
+        incididunt quis adipisicing ipsum laboris Lorem ullamco cupidatat ipsum. Nisi quis eu laborum dolore veniam quis nostrud
+        ullamco. Nulla ullamco sit nisi ea nostrud dolor ea aute.
+      </Text>
+    </ScrollView>
   );
 }
